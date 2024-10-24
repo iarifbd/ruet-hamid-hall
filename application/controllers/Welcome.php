c02a031c71ca7e1773e30d807cd543eeff0cd234
iarifbd <iarifbd@gmail.com>
2024-10-24 09:06:41 +0600

    update as required for this page
    update as required for this page

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -10,66 +10,11 @@ class Welcome extends CI_Controller{
 	}
 
 	public function index() {
-	    $data['area_chart_data'] = $this->AreaChart();
-	    $data['bar_chart_data'] = $this->BarChat();
-	    $this->load->view('template/template', $data);
+
+	    $this->load->view('template/template');
 	}
 
-    public function AreaChart()
-    {
-        // Fetch data from the AreaChart model
-        $sales_data  = $this->AreaChart->getMonthlyNetAmounts();
 
-
-        // Prepare data for Chart.js
-        $labels = [];
-        $data = [];
-
-        foreach ($sales_data as $row) {
-            $labels[] = $row['month_name'];
-            $data[] = $row['total_amount'];
-        }
-
-        // Pass data to view
-        $data = json_encode([
-            'labels' => $labels,
-            'datasets' => [[
-                'label' => "Sales Amount",
-                'backgroundColor' => "rgba(75,192,192,0.4)",
-                'borderColor' => "rgba(75,192,192,1)",
-                'data' => $data,
-                'fill' => true,
-            ]]
-        ]);
-
-        return $data;
-    }
-
-	public function BarChat(){
-	    // SQL Query Builder to fetch month name and total amount
-	    $this->db->select("DATE_FORMAT(date_time, '%b') AS month_name, SUM(net_amount) AS total_amount");
-	    $this->db->from('orders');
-	    $this->db->group_by("DATE_FORMAT(date_time, '%b')");
-	    $this->db->order_by('MIN(date_time)');
-
-	    $query = $this->db->get();
-	    $result = $query->result_array();
-
-	    // Prepare data for Chart.js
-	    $labels = [];
-	    $data = [];
-
-	    foreach ($result as $row) {
-	        $labels[] = $row['month_name']; // e.g., "Jan", "Feb"
-	        $data[] = $row['total_amount']; // e.g., 12345
-	    }
-
-	    // Return data as an associative array
-	    return [
-	        'labels' => $labels,
-	        'data' => $data,
-	    ];
-	}
 
 
 
--- application/views/template/sidenav.php
+++ application/views/template/sidenav.php
@@ -7,63 +7,7 @@
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
-                            <div class="sb-sidenav-menu-heading">Interface</div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fa fa-industry"></i></div>
-                                Products
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <!-- <a class="nav-link" href="<?php echo base_url('Products/add_product'); ?>">Add New Product</a> -->
-                                    <a class="nav-link" href="<?php echo base_url('Products'); ?>">Manage Products</a>
-                                    <a class="nav-link" href="<?php echo base_url('Products/settings'); ?>">Settings</a>
-                                </nav>
-                            </div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#StoreLayout" aria-expanded="false" aria-controls="StoreLayout">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Store
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="StoreLayout" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="<?php echo base_url('Store/add'); ?>">Add New Product</a>
-                                    <a class="nav-link" href="<?php echo base_url('Store/storeInfo'); ?>">Edit info</a>
-                                    <a class="nav-link" href="<?php echo base_url('Store'); ?>">Store Observe</a>
-                                    <a class="nav-link" href="<?php echo base_url('Store/short_list'); ?>">Short List</a>
-                                    <a class="nav-link" href="<?php echo base_url('Store/expired_list'); ?>">Expire List</a>
-                                </nav>
-                            </div>
-
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                <div class="sb-nav-link-icon"><i class="fas fa-book-open"></i></div>
-                                Quotation
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="<?php echo base_url('Quotetion'); ?>">Create</a>
-                                    <a class="nav-link" href="<?php echo base_url('Quotetion/list'); ?>">Manage Quotetion</a>
-                                    <a class="nav-link" href="<?php echo base_url('Editor'); ?>">Manage T&C</a>
-                                </nav>
-                            </div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#InvoicePages" aria-expanded="false" aria-controls="InvoicePages">
-                                <div class="sb-nav-link-icon"><i class="fa-solid fa-file-invoice"></i></div>
-                                Invoice
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="InvoicePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="<?php echo base_url('Invoice'); ?>">Create</a>
-                                    
-                                </nav>
-                            </div>
-                            <div class="sb-sidenav-menu-heading">Reports</div>
-                            <a class="nav-link" href="<?php echo base_url('Sales'); ?>">
-                                <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
-                                Sales Report
-                            </a>
-                            
+                            <div class="sb-sidenav-menu-heading">Interface</div>                
                         </div>
                     </div>
                     <div class="sb-sidenav-footer">
